b321fc87e7bec55577fb66d7f7c6bcbbf692da66
nairx <nairxprv@gmail.com>
2026-02-07 14:39:48 +0530

    updated

---
+++ file10.js
@@ -0,0 +1,12 @@
+// let a = 10
+// console.log(a)
+const a = function greet() {
+    console.log("Good Morning")
+}
+a()
+
+//arrow function
+const b = () => {
+    console.log("Good Evening")
+}
+b()
