e9458c4419dc489789fedf38fb2258b963d0f806
nairx <nairxprv@gmail.com>
2026-02-07 14:26:43 +0530

    updated

---
+++ file9.js
@@ -0,0 +1,25 @@
+//functions
+// function greet(){
+//     console.log("Good Day!")
+// }
+// greet()
+
+//passing arguments
+// function greet(name){
+//     console.log("Hello " + name)
+// }
+// greet("John")
+// greet("Cathy")
+
+//pass two arguments
+// function add(x,y){
+//     console.log(x+y)
+// }
+// add(5,6)
+
+//passing arguments and returning value
+function add(x,y){
+    return x+y
+}
+let result = add(4,5)
+console.log(result)
