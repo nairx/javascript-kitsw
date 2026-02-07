aeccc7ff098c8fbda7d413b5ebce4fe7da78950c
nairx <nairxprv@gmail.com>
2026-02-07 13:08:07 +0530

    updated

--- file4.js
+++ file4.js
@@ -1,5 +1,5 @@
-let a = 10
-let b = 3
+// let a = 10
+// let b = 3
 //Arithmetic Operator
 // console.log(a+b)
 // console.log(a-b)
@@ -13,8 +13,16 @@ let b = 3
 // console.log(a>=b)
 // console.log(a<b)
 // console.log(a<=b)
-a = "10"
-b = 10
-console.log(a==b) //check for value
-console.log(a===b) //check for value and type
+// a = "10"
+// b = 10
+// console.log(a==b) //check for value
+// console.log(a===b) //check for value and type
 // console.log(a!=b)
+
+
+let x = 5
+let y = 2
+
+console.log(x>y && x>10)
+
+console.log(x>y || x>10)
