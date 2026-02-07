82679bcf8dace72883ea737289cc234791bc73ca
nairx <nairxprv@gmail.com>
2026-02-07 15:54:07 +0530

    updated

--- file17.js
+++ file17.js
@@ -6,8 +6,22 @@
 // console.log(newArray)
 
 //Modified value is returned
-const numbers = [5,7,3,2]
-const newArray = numbers.map((value=>{
-    return value + 2
-}))
-console.log(newArray)
+// const numbers = [5,7,3,2]
+// const newArray = numbers.map((value=>{
+//     return value + 2
+// }))
+// console.log(newArray)
+
+//Same value but after filter is returned
+// const numbers = [5,7,3,2]
+// const newArray = numbers.filter((value)=>{
+//     return value > 3
+// })
+// console.log(newArray)
+
+//First matching value is returned
+const rollNumber = [35,37,33,22]
+const result = rollNumber.find((value)=>{
+    return value === 35
+})
+console.log(result)
