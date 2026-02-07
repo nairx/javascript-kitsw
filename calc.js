7783ba9c6719d1d25608bd56762c09229cdf141f
nairx <nairxprv@gmail.com>
2026-02-07 16:39:30 +0530

    updated

---
+++ calc.js
@@ -0,0 +1,13 @@
+function add(x,y){
+    return x+y
+}
+
+function subtract(x,y){
+    return x-y
+}
+
+// to export just one function
+// export default add
+
+// to export multiple function
+export {add,subtract}
---
+++ file19.js
@@ -0,0 +1,8 @@
+// import add from "./calc.js"
+import {add,subtract} from "./calc.js"
+
+const result = add(4,5)
+console.log(result)
+
+const result1 = subtract(7,2)
+console.log(result1)
