5ac0c09627433274f4a3c6d9196126e7b6700eaf
nairx <nairxprv@gmail.com>
2026-02-07 13:03:31 +0530

    updated

---
+++ file4.js
@@ -0,0 +1,20 @@
+let a = 10
+let b = 3
+//Arithmetic Operator
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
+//Comparison Operator
+// console.log(a>b)
+// console.log(a>=b)
+// console.log(a<b)
+// console.log(a<=b)
+a = "10"
+b = 10
+console.log(a==b) //check for value
+console.log(a===b) //check for value and type
+// console.log(a!=b)
