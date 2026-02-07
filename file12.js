c8d393481a854475c72df7bae89d9273940a2bd2
nairx <nairxprv@gmail.com>
2026-02-07 14:55:21 +0530

    updated

--- file12.js
+++ file12.js
@@ -2,9 +2,25 @@
 const greet = () => {
   console.log("Hello World");
 };
+
+const greet1 = () => {
+  console.log("Good Morning");
+};
+
+const greet2 = () => {
+    console.log("Good Evening")
+}
+
+const greet3 = () => {
+    console.log("Good Afternoon")
+}
+
+
 const main = (f) => {
   f();
 };
+
 main(greet);
-
-
+main(greet1);
+main(greet2);
+main(greet3);
