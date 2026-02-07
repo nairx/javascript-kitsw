29035f87f70f2ac2bde1837d7d672330a47213e4
nairx <nairxprv@gmail.com>
2026-02-07 13:56:47 +0530

    updated

---
+++ file5.js
@@ -0,0 +1,17 @@
+// const a = 10
+// a = 23 //will throw error because
+// value of const cannot be changed
+
+
+let x = 5
+let y = 5
+
+if (x>y){
+    console.log("x is greater")
+}
+else if(y>x){
+    console.log("y is greater")
+}
+else{
+    console.log("Both are equal")
+}
