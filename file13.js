2a1395cbe1c28b72498d2e6c2f63bb263a4f493f
nairx <nairxprv@gmail.com>
2026-02-07 15:05:03 +0530

    updated

---
+++ file13.js
@@ -0,0 +1,14 @@
+const student = {
+  name: "John",
+  age: 21,
+};
+// console.log(student)
+// console.log(student.name)
+student["city"] = "NYC"
+console.log(student)
+
+//create an object to store
+//order details
+//orderID,customerName,ProductName,Price
+
+
