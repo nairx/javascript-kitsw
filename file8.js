f59adb4c9b219924ecd94c74c58bebfb8ffb68c0
nairx <nairxprv@gmail.com>
2026-02-07 14:17:52 +0530

    updated

---
+++ file8.js
@@ -0,0 +1,19 @@
+//break and continue statement
+// let i = 1
+// while (i<=5){
+//     console.log(i)
+//     i++
+//     if (i==4){
+//         break //break the loop
+//     }
+// }
+
+
+let i = 0
+while (i<=5){
+    i++
+    if (i==4){
+        continue  //break the iteration and go to next one
+    }
+    console.log(i)
+}
